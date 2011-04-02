3dfe7e73f781b7c3decccbbc61593dba1873b90b
edashofy <edashofy@4f589ee1-8d18-4a0b-a29f-e26e9080690b>
2011-04-02 00:25:59 +0000

    Rename some variables I forgot to earlier.
    git-svn-id: http://www.archstudio.org/svn/main/5/trunk@45 4f589ee1-8d18-4a0b-a29f-e26e9080690b

--- org.archstudio.xarchadt.common/src/org/archstudio/xarchadt/common/BasicXArchADTSubstitutionHint.java
+++ org.archstudio.xarchadt.common/src/org/archstudio/xarchadt/common/BasicXArchADTSubstitutionHint.java
@@ -2,27 +2,27 @@ package org.archstudio.xarchadt.common;
 
 public class BasicXArchADTSubstitutionHint implements IXArchADTSubstitutionHint {
 	
-	protected String extensionNsURI;
-	protected String extensionTypeName;
+	protected String sourceNsURI;
+	protected String sourceTypeName;
 	
 	protected String targetNsURI;
 	protected String targetTypeName;
 	
-	public BasicXArchADTSubstitutionHint(String extensionNsURI, String extensionTypeName, String targetNsURI, String targetTypeName) {
-		this.extensionNsURI = extensionNsURI;
-		this.extensionTypeName = extensionTypeName;
+	public BasicXArchADTSubstitutionHint(String sourceNsURI, String sourceTypeName, String targetNsURI, String targetTypeName) {
+		this.sourceNsURI = sourceNsURI;
+		this.sourceTypeName = sourceTypeName;
 		this.targetNsURI = targetNsURI;
 		this.targetTypeName = targetTypeName;
 	}
 
 	@Override
 	public String getSourceNsURI() {
-		return extensionNsURI;
+		return sourceNsURI;
 	}
 
 	@Override
 	public String getSourceTypeName() {
-		return extensionTypeName;
+		return sourceTypeName;
 	}
 
 	@Override
