86fafa0a7b5b2e0ce56895fb90a0d9bcc593171f
edashofy <edashofy@4f589ee1-8d18-4a0b-a29f-e26e9080690b>
2011-08-18 00:11:59 +0000

    Remove unused import
    git-svn-id: http://www.archstudio.org/svn/main/5/trunk@136 4f589ee1-8d18-4a0b-a29f-e26e9080690b

--- org.archstudio.bna/src/org/archstudio/bna/facets/IHasMidpoints.java
+++ org.archstudio.bna/src/org/archstudio/bna/facets/IHasMidpoints.java
@@ -3,7 +3,6 @@ package org.archstudio.bna.facets;
 import java.util.List;
 
 import org.archstudio.bna.IThing;
-import org.archstudio.bna.IThing.IThingKey;
 import org.archstudio.bna.keys.AbstractCloneThingKey;
 import org.archstudio.bna.keys.AbstractCollectionThingKey;
 import org.archstudio.bna.keys.CollectionThingKey;
