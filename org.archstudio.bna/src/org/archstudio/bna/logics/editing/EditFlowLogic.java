1dc2d753bbdf61c3264986e6096ac4798bc56509
sahendrickson <sahendrickson@4f589ee1-8d18-4a0b-a29f-e26e9080690b>
2011-08-24 18:08:48 +0000

    Restored ability to edit interface directions
    git-svn-id: http://www.archstudio.org/svn/main/5/trunk@157 4f589ee1-8d18-4a0b-a29f-e26e9080690b

---
+++ org.archstudio.bna/src/org/archstudio/bna/logics/editing/EditFlowLogic.java
@@ -0,0 +1,54 @@
+package org.archstudio.bna.logics.editing;
+
+import org.archstudio.bna.IBNAView;
+import org.archstudio.bna.ICoordinate;
+import org.archstudio.bna.IThing;
+import org.archstudio.bna.facets.IHasMutableFlow;
+import org.archstudio.bna.logics.AbstractThingLogic;
+import org.archstudio.bna.utils.Assemblies;
+import org.archstudio.bna.utils.BNAUtils;
+import org.archstudio.bna.utils.IBNAMenuListener;
+import org.archstudio.bna.utils.UserEditableUtils;
+import org.archstudio.swtutils.constants.Flow;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IMenuManager;
+import org.eclipse.jface.action.MenuManager;
+
+import com.google.common.collect.Iterables;
+
+public class EditFlowLogic extends AbstractThingLogic implements IBNAMenuListener {
+
+	public EditFlowLogic() {
+	}
+
+	@Override
+	public void fillMenu(final IBNAView view, Iterable<IThing> things, final ICoordinate location, IMenuManager menu) {
+		IThing editThing = null;
+		if (Iterables.size(BNAUtils.getSelectedThings(view.getBNAWorld().getBNAModel())) <= 1) {
+			MAIN: for (IThing thing : things) {
+				for (IThing assemblyPartThing : Assemblies.getRelatedParts(view.getBNAWorld().getBNAModel(), thing)) {
+					if (assemblyPartThing instanceof IHasMutableFlow
+							&& UserEditableUtils.isEditableForAnyQualities(assemblyPartThing,
+									IHasMutableFlow.USER_MAY_EDIT_FLOW)) {
+						editThing = assemblyPartThing;
+						break MAIN;
+					}
+				}
+			}
+		}
+		final IThing finalThing = editThing;
+		if (finalThing != null) {
+			MenuManager editDirectionMenu = new MenuManager("Edit Direction...");
+			for (final Flow f : Flow.values()) {
+				editDirectionMenu.add(new Action(f.toString()) {
+					@Override
+					public void run() {
+						finalThing.set(IHasMutableFlow.FLOW_KEY, f);
+					}
+				});
+			}
+			menu.add(editDirectionMenu);
+		}
+	}
+
+}
--- org.archstudio.utils.swt/src/org/archstudio/swtutils/constants/Flow.java
+++ org.archstudio.utils.swt/src/org/archstudio/swtutils/constants/Flow.java
@@ -1,5 +1,16 @@
 package org.archstudio.swtutils.constants;
 
 public enum Flow {
-	NONE, IN, OUT, INOUT
+	NONE("None"), IN("In"), OUT("Out"), INOUT("In-Out");
+	
+	private final String description;
+
+	private Flow(String description) {
+		this.description = description;
+	}
+	
+	@Override
+	public String toString() {
+		return description;
+	}
 }
