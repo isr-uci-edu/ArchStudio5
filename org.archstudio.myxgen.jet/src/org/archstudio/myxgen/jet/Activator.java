9f5ccaa80abef23e7ead12547311e83c9918b014
sahendrickson <sahendrickson@4f589ee1-8d18-4a0b-a29f-e26e9080690b>
2011-09-03 15:45:39 +0000

    Fixed NPE in MyxGen
    git-svn-id: http://www.archstudio.org/svn/main/5/trunk@169 4f589ee1-8d18-4a0b-a29f-e26e9080690b

---
+++ org.archstudio.myxgen.jet/src/org/archstudio/myxgen/jet/Activator.java
@@ -0,0 +1,17 @@
+package org.archstudio.myxgen.jet;
+
+
+import org.archstudio.utils.osgi.AbstractActivator;
+
+public class Activator extends AbstractActivator {
+
+	private static Activator singleton;
+
+	public Activator() {
+		singleton = this;
+	}
+
+	public static final Activator getSingleton() {
+		return singleton;
+	}
+}
