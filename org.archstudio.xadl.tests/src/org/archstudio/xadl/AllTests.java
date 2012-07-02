d316f1f6ba4c3fa8d595725dcc91b4e8adccc6b4
Scott A. Hendrickson <sahendrickson@gmail.com>
2012-07-01 22:02:26 -0700

    Organized tests to be executed from a single point

---
+++ org.archstudio.xadl.tests/src/org/archstudio/xadl/AllTests.java
@@ -0,0 +1,11 @@
+package org.archstudio.xadl;
+
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+import org.junit.runners.Suite.SuiteClasses;
+
+@RunWith(Suite.class)
+@SuiteClasses({ XArchRelativePathTrackerTest.class })
+public class AllTests {
+
+}
