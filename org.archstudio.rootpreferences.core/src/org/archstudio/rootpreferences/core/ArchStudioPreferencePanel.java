5eda1fd1f52a859b012af0f6de558091fac2f2ef
Scott A. Hendrickson <sahendrickson@gmail.com>
2012-07-15 09:13:59 -0700

    Removed root preferences

--- org.archstudio.rootpreferences.core/src/org/archstudio/rootpreferences/core/ArchStudioPreferencePanel.java
+++ org.archstudio.rootpreferences.core/src/org/archstudio/rootpreferences/core/ArchStudioPreferencePanel.java
@@ -1,14 +1,9 @@
 package org.archstudio.rootpreferences.core;
 
-import java.io.IOException;
-
 import org.archstudio.eclipse.core.startup.InstantiateArchStudio;
-import org.archstudio.myx.fw.MyxRegistry;
-import org.archstudio.resources.IResources;
-import org.archstudio.sysutils.SystemUtils;
-import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.graphics.Image;
 import org.eclipse.swt.layout.GridLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
@@ -18,32 +13,16 @@ import org.eclipse.ui.IWorkbenchPreferencePage;
 
 public class ArchStudioPreferencePanel extends PreferencePage implements IWorkbenchPreferencePage {
 
-	public static final String IMAGE_PREFERENCES_BANNER = "archstudio:preferences/banner";
-
-	private RootPreferencesMyxComponent comp = null;
-	private final MyxRegistry er = MyxRegistry.getSharedInstance();
-
-	protected IResources resources = null;
-	protected IPreferenceStore preferences = null;
-
+	protected Image image = null;
+	
 	public ArchStudioPreferencePanel() {
 		super("ArchStudio Preferences");
 		InstantiateArchStudio.instantiate();
-		comp = (RootPreferencesMyxComponent) er.waitForBrick(RootPreferencesMyxComponent.class);
-		er.map(comp, this);
-		resources = comp.resources;
-		preferences = comp.preferences;
-		try {
-			resources.createImage(IMAGE_PREFERENCES_BANNER,
-					SystemUtils.blt(ArchStudioPreferencePanel.class.getResourceAsStream("res/banner.png")));
-		}
-		catch (IOException ioe) {
-			throw new RuntimeException("This shouldn't happen.");
-		}
 	}
 
 	@Override
 	public void init(IWorkbench workbench) {
+		image = new Image(workbench.getDisplay(), ArchStudioPreferencePanel.class.getResourceAsStream("res/banner.png"));
 	}
 
 	@Override
@@ -52,11 +31,18 @@ public class ArchStudioPreferencePanel extends PreferencePage implements IWorkbe
 		c.setLayout(new GridLayout(1, false));
 
 		Label lBanner = new Label(c, SWT.BORDER);
-		lBanner.setImage(resources.getImage(IMAGE_PREFERENCES_BANNER));
+		lBanner.setImage(image);
 
 		Label lText = new Label(c, SWT.LEFT);
 		lText.setText("Select a sub-node for options.");
 		return c;
+	}
+	
+	@Override
+	public void dispose() {
+		image.dispose();
+		image = null;
+		super.dispose();
 	}
 
 }
