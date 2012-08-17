a6430c71a6b8d8a0c327e481e9cf1ad8723950bf
Scott A. Hendrickson <sahendrickson@gmail.com>
2012-08-17 09:03:17 -0700

    Created a temporary extension point for experiments

--- org.archstudio.archipelago.core/src/org/archstudio/archipelago/core/RootTreePlugin.java
+++ org.archstudio.archipelago.core/src/org/archstudio/archipelago/core/RootTreePlugin.java
@@ -1,11 +1,17 @@
 package org.archstudio.archipelago.core;
 
 import org.archstudio.myx.fw.Services;
+import org.archstudio.xarchadt.IXArchADT;
 import org.archstudio.xarchadt.ObjRef;
+import org.archstudio.xarchadt.XArchADTProxy;
+import org.eclipse.emf.ecore.EObject;
+import org.eclipse.jface.action.Action;
+import org.eclipse.jface.action.IAction;
+import org.eclipse.jface.action.IMenuManager;
 import org.eclipse.jface.viewers.TreeViewer;
 
 public class RootTreePlugin extends AbstractArchipelagoTreePlugin {
-	public RootTreePlugin(TreeViewer viewer, Services AS, ObjRef documentRootRef) {
+	public RootTreePlugin(TreeViewer viewer, final Services AS, ObjRef documentRootRef) {
 		this.contentProvider = new RootContentProvider(AS, documentRootRef);
 		this.labelProvider = new RootLabelProvider(AS);
 
@@ -16,6 +22,24 @@ public class RootTreePlugin extends AbstractArchipelagoTreePlugin {
 				fviewer.setSelection(null);
 			}
 		};
+
+		this.contextMenuFillers = new IArchipelagoTreeContextMenuFiller[] { new IArchipelagoTreeContextMenuFiller() {
+			@Override
+			public void fillContextMenu(IMenuManager m, Object[] selectedNodes) {
+				if ((selectedNodes != null) && (selectedNodes.length == 1)) {
+					final Object selectedNode = selectedNodes[0];
+					IAction newStructureAction = new Action("Temporary Entry Point") {
+						public void run() {
+							process(XArchADTProxy.<EObject> proxy(AS.get(IXArchADT.class), (ObjRef) selectedNode));
+						}
+					};
+					m.add(newStructureAction);
+				}
+			}
+		} };
 	}
 
+	public void process(EObject eObject) {
+		// extend here...
+	}
 }
