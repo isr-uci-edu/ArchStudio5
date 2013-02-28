05b158c2b594dbb1c4ea28d304e3451554c3c9b8
Scott A. Hendrickson <sahendrickson@gmail.com>
2013-02-27 16:55:35 -0800

    Fixed: was difficult to select horizontal and vertical splines

--- org.archstudio.bna/src/org/archstudio/bna/things/AbstractSplineThingPeer.java
+++ org.archstudio.bna/src/org/archstudio/bna/things/AbstractSplineThingPeer.java
@@ -8,6 +8,7 @@ import org.archstudio.bna.ICoordinate;
 import org.archstudio.bna.ICoordinateMapper;
 import org.archstudio.bna.utils.BNAUtils;
 import org.eclipse.swt.graphics.Point;
+import org.eclipse.swt.graphics.Rectangle;
 
 public abstract class AbstractSplineThingPeer<T extends AbstractSplineThing> extends AbstractThingPeer<T> {
 
@@ -17,14 +18,22 @@ public abstract class AbstractSplineThingPeer<T extends AbstractSplineThing> ext
 
 	@Override
 	public boolean isInThing(IBNAView view, ICoordinateMapper cm, ICoordinate location) {
-		Point lp = location.getLocalPoint();
-		List<Point> points = BNAUtils.worldToLocal(cm, t.getPoints());
-		for (int i = 1; i < points.size(); i++) {
-			Point p0 = points.get(i - 1);
-			Point p1 = points.get(i);
-			double distSq = Line2D.ptSegDistSq(p0.x, p0.y, p1.x, p1.y, lp.x, lp.y);
-			if (distSq <= 25) {
-				return true;
+		int distance = 5;
+		Rectangle lbb = BNAUtils.getLocalBoundingBox(cm, t);
+		lbb.x -= distance;
+		lbb.y -= distance;
+		lbb.width += distance * 2;
+		lbb.height += distance * 2;
+		if (lbb.contains(location.getLocalPoint())) {
+			Point lp = location.getLocalPoint();
+			List<Point> points = BNAUtils.worldToLocal(cm, t.getPoints());
+			for (int i = 1; i < points.size(); i++) {
+				Point p0 = points.get(i - 1);
+				Point p1 = points.get(i);
+				double distSq = Line2D.ptSegDistSq(p0.x, p0.y, p1.x, p1.y, lp.x, lp.y);
+				if (distSq <= distance * distance) {
+					return true;
+				}
 			}
 		}
 		return false;
