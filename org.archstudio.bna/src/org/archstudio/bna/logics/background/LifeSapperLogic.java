0a62063d2bb2a862e0de8e9dacf2686c9bc113d8
Scott A. Hendrickson <sahendrickson@gmail.com>
2013-10-05 10:50:59 -0700

    Modified LifeSapperLogic and RotatingOffsetLogic to use a single thread

--- org.archstudio.bna/src/org/archstudio/bna/logics/background/LifeSapperLogic.java
+++ org.archstudio.bna/src/org/archstudio/bna/logics/background/LifeSapperLogic.java
@@ -1,76 +1,76 @@
 package org.archstudio.bna.logics.background;
 
+import java.util.List;
+
 import org.archstudio.bna.IBNAWorld;
-import org.archstudio.bna.IThing;
 import org.archstudio.bna.facets.IHasMutableLife;
 import org.archstudio.bna.logics.AbstractThingLogic;
 import org.archstudio.bna.logics.tracking.ThingTypeTrackingLogic;
+import org.archstudio.swtutils.SWTWidgetUtils;
 import org.eclipse.swt.widgets.Display;
 
-public class LifeSapperLogic extends AbstractThingLogic {
+import com.google.common.collect.Lists;
 
-	protected final ThingTypeTrackingLogic typeLogic;
-	protected final LifeSapper sapper;
+public final class LifeSapperLogic extends AbstractThingLogic {
 
-	public LifeSapperLogic(IBNAWorld world) {
-		super(world);
-		typeLogic = logics.addThingLogic(ThingTypeTrackingLogic.class);
-		sapper = new LifeSapper();
-		sapper.setName(this.getClass().toString());
-		sapper.setDaemon(true);
-		sapper.start();
-	}
-
-	@Override
-	synchronized public void dispose() {
-		if (sapper != null) {
-			sapper.terminate();
-		}
-		super.dispose();
-	}
-
-	public class LifeSapper extends Thread {
-
-		protected volatile boolean shouldTerminate = false;
-
-		protected LifeSapper() {
-		}
-
-		public synchronized void terminate() {
-			shouldTerminate = true;
-		}
-
-		@Override
-		public void run() {
-			while (!shouldTerminate) {
-				try {
-					Thread.sleep(125);
-				}
-				catch (InterruptedException e) {
-				}
-
-				Display.getDefault().syncExec(new Runnable() {
-					@Override
-					public void run() {
-						model.beginBulkChange();
-						try {
-							for (final IThing t : model.getThingsByID(typeLogic.getThingIDs(IHasMutableLife.class))) {
-								if (t instanceof IHasMutableLife) {
-									final IHasMutableLife tl = (IHasMutableLife) t;
-									int life;
-									tl.setLife(life = tl.getLife() - 1);
-									if (life < 0) {
-										model.removeThing(tl);
+	private static List<IBNAWorld> worlds = Lists.newCopyOnWriteArrayList();
+	private static Thread updater;
+	static {
+		updater = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(125);
+					}
+					catch (InterruptedException e) {
+					}
+					SWTWidgetUtils.sync(Display.getDefault(), new Runnable() {
+						@Override
+						public void run() {
+							for (IBNAWorld world : worlds) {
+								boolean changedSomething = false;
+								try {
+									ThingTypeTrackingLogic typesLogic = world.getThingLogicManager().addThingLogic(
+											ThingTypeTrackingLogic.class);
+									for (IHasMutableLife t : typesLogic.getThings(IHasMutableLife.class)) {
+										int life = t.getLife() - 1;
+										if (life <= 0) {
+											world.getBNAModel().removeThing(t);
+										}
+										else {
+											if (!changedSomething) {
+												world.getBNAModel().beginBulkChange();
+												changedSomething = true;
+											}
+											t.setLife(life);
+										}
+									}
+								}
+								finally {
+									if (changedSomething) {
+										world.getBNAModel().endBulkChange();
 									}
 								}
 							}
 						}
-						finally {
-							model.endBulkChange();
-						}
-					}
-				});
+					});
+				}
 			}
-		}
+		});
+		updater.setName(LifeSapperLogic.class.getName());
+		updater.setDaemon(true);
+		updater.start();
+	}
+
+	public LifeSapperLogic(IBNAWorld world) {
+		super(world);
+		worlds.add(world);
+	}
+
+	@Override
+	synchronized public void dispose() {
+		worlds.remove(world);
+		super.dispose();
 	}
 }
--- org.archstudio.bna/src/org/archstudio/bna/logics/background/RotatingOffsetLogic.java
+++ org.archstudio.bna/src/org/archstudio/bna/logics/background/RotatingOffsetLogic.java
@@ -1,70 +1,79 @@
 package org.archstudio.bna.logics.background;
 
+import java.util.List;
+
 import org.archstudio.bna.IBNAWorld;
 import org.archstudio.bna.facets.IHasMutableRotatingOffset;
 import org.archstudio.bna.logics.AbstractThingLogic;
+import org.archstudio.bna.logics.tracking.ThingTypeTrackingLogic;
+import org.archstudio.swtutils.SWTWidgetUtils;
 import org.eclipse.swt.widgets.Display;
 
-import com.google.common.collect.Iterables;
+import com.google.common.collect.Lists;
 
-public class RotatingOffsetLogic extends AbstractThingLogic {
+public final class RotatingOffsetLogic extends AbstractThingLogic {
 
-	protected final RotatingOffsetIncrementer timer;
+	protected static final boolean DEBUG = false;
+	private static List<IBNAWorld> worlds = Lists.newCopyOnWriteArrayList();
+	private static Thread updater;
+	static {
+		updater = new Thread(new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Thread.sleep(125);
+					}
+					catch (InterruptedException e) {
+					}
+
+					if (DEBUG) {
+						continue;
+					}
+					SWTWidgetUtils.sync(Display.getDefault(), new Runnable() {
+						@Override
+						public void run() {
+							for (IBNAWorld world : worlds) {
+								boolean changedSomething = false;
+								try {
+									ThingTypeTrackingLogic typesLogic = world.getThingLogicManager().addThingLogic(
+											ThingTypeTrackingLogic.class);
+									for (IHasMutableRotatingOffset t : typesLogic
+											.getThings(IHasMutableRotatingOffset.class)) {
+										if (t.shouldIncrementRotatingOffset()) {
+											if (!changedSomething) {
+												world.getBNAModel().beginBulkChange();
+												changedSomething = true;
+											}
+											t.incrementRotatingOffset();
+										}
+									}
+								}
+								finally {
+									if (changedSomething) {
+										world.getBNAModel().endBulkChange();
+									}
+								}
+							}
+						}
+					});
+				}
+			}
+
+		});
+		updater.setName(RotatingOffsetLogic.class.getName());
+		updater.setDaemon(true);
+		updater.start();
+	}
 
 	public RotatingOffsetLogic(IBNAWorld world) {
 		super(world);
-		timer = new RotatingOffsetIncrementer();
-		timer.setName(this.getClass().getName());
-		timer.setDaemon(true);
-		timer.start();
+		worlds.add(world);
 	}
 
 	@Override
 	synchronized public void dispose() {
-		if (timer != null) {
-			timer.terminate();
-		}
+		worlds.remove(world);
 		super.dispose();
-	}
-
-	public class RotatingOffsetIncrementer extends Thread {
-
-		protected volatile boolean shouldTerminate = false;
-
-		protected RotatingOffsetIncrementer() {
-		}
-
-		public synchronized void terminate() {
-			shouldTerminate = true;
-		}
-
-		@Override
-		public void run() {
-			while (!shouldTerminate) {
-				try {
-					Thread.sleep(125);
-				}
-				catch (InterruptedException e) {
-				}
-
-				Display.getDefault().syncExec(new Runnable() {
-					@Override
-					public void run() {
-						model.beginBulkChange();
-						try {
-							for (IHasMutableRotatingOffset t : Iterables.filter(model.getAllThings(),
-									IHasMutableRotatingOffset.class)) {
-								if (t.shouldIncrementRotatingOffset()) {
-									t.incrementRotatingOffset();
-								}
-							}
-						}
-						finally {
-							model.endBulkChange();
-						}
-					}
-				});
-			}
-		}
 	}
 }
