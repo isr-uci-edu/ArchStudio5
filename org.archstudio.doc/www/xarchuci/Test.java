1623df3d53f3eb8e7459fd070b2d0e581c4bdbfe
Scott A. Hendrickson <sahendrickson@gmail.com>
2013-10-08 10:32:27 -0700

    Updated github references to reflect new name

--- org.archstudio.doc/www/xarchuci/Test.java
+++ org.archstudio.doc/www/xarchuci/Test.java
@@ -1,2 +1,2 @@
 The test example has moved to the following location:
-	https://github.com/isr-uci-edu/ArchStudio/blob/master/org.archstudio.demo.xarchuci/src/org/archstudio/demo/xarchuci/Test.java
+	https://github.com/isr-uci-edu/ArchStudio5/blob/master/org.archstudio.demo.xarchuci/src/org/archstudio/demo/xarchuci/Test.java
