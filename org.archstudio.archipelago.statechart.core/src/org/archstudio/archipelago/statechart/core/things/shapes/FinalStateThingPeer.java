e5615e1cb152abc6712727eccbbf9781cf630a2f
Scott A. Hendrickson <sahendrickson@gmail.com>
2013-12-26 15:24:05 -0800

    Updated look of FinalStateThing

--- org.archstudio.archipelago.statechart.core/src/org/archstudio/archipelago/statechart/core/things/shapes/FinalStateThingPeer.java
+++ org.archstudio.archipelago.statechart.core/src/org/archstudio/archipelago/statechart/core/things/shapes/FinalStateThingPeer.java
@@ -8,9 +8,12 @@ import org.archstudio.bna.ICoordinateMapper;
 import org.archstudio.bna.things.shapes.EllipseThingPeer;
 import org.archstudio.bna.ui.IUIResources;
 import org.archstudio.bna.utils.BNAUtils;
+import org.archstudio.sysutils.SystemUtils;
+import org.eclipse.swt.graphics.RGB;
 import org.eclipse.swt.graphics.Rectangle;
 
-public class FinalStateThingPeer<T extends FinalStateThing> extends EllipseThingPeer<T> {
+public class FinalStateThingPeer<T extends FinalStateThing> extends
+		EllipseThingPeer<T> {
 
 	public FinalStateThingPeer(T thing, IBNAView view, ICoordinateMapper cm) {
 		super(thing, view, cm);
@@ -23,11 +26,17 @@ public class FinalStateThingPeer<T extends FinalStateThing> extends EllipseThing
 			return false;
 		}
 
-		Shape outerShape = new Ellipse2D.Double(lbb.x, lbb.y, lbb.width, lbb.height);
-		Shape innerShape = new Ellipse2D.Double(lbb.x + 4f, lbb.y + 4f, lbb.width - 8, lbb.height - 8);
+		Shape outerShape = new Ellipse2D.Double(lbb.x, lbb.y, lbb.width,
+				lbb.height);
+		float delta = SystemUtils.bound(5, 8 * (float) cm.getLocalScale(),
+				Float.MAX_VALUE);
+		Shape innerShape = new Ellipse2D.Double(lbb.x + delta, lbb.y + delta,
+				lbb.width - 2 * delta, lbb.height - 2 * delta);
 
+		r.fillShape(outerShape, new RGB(255, 255, 255), null);
 		if (t.getColor() != null) {
-			r.fillShape(innerShape, t.getColor(), t.isGradientFilled() ? t.getSecondaryColor() : null);
+			r.fillShape(innerShape, t.getColor(),
+					t.isGradientFilled() ? t.getSecondaryColor() : null);
 		}
 		if (r.setLineStyle(t)) {
 			r.drawShape(outerShape);
