666ab929e08700632d60b43c917bfa909d0b41a2
Scott A. Hendrickson <sahendrickson@gmail.com>
2015-07-22 08:08:06 -0700

    Fixed: MyxRegistry weak references were not removed correctly.

--- org.archstudio.myx.fw/src/org/archstudio/myx/fw/MyxRegistry.java
+++ org.archstudio.myx.fw/src/org/archstudio/myx/fw/MyxRegistry.java
@@ -10,6 +10,7 @@ import java.util.concurrent.CopyOnWriteArrayList;
 
 import org.archstudio.myx.fw.MyxRegistryEvent.EventType;
 
+import com.google.common.base.Preconditions;
 import com.google.common.collect.Lists;
 
 /**
@@ -47,13 +48,14 @@ public final class MyxRegistry {
 	 * @param brick The brick to register.
 	 */
 	public void registerBrick(IMyxBrick brick) {
+		Preconditions.checkNotNull(brick);
 		synchronized (this) {
 			if (brickToObjectsMap.get(brick) == null) {
 				brickToObjectsMap.put(brick, new CopyOnWriteArrayList<WeakReference<Object>>());
 			}
 			notifyAll();
+			fireMyxRegistryEvent(MyxRegistryEvent.EventType.BRICK_REGISTERED, brick, null);
 		}
-		fireMyxRegistryEvent(MyxRegistryEvent.EventType.BRICK_REGISTERED, brick, null);
 	}
 
 	/**
@@ -70,6 +72,7 @@ public final class MyxRegistry {
 	 * @param brick The brick to unregister.
 	 */
 	public void unregisterBrick(IMyxBrick brick) {
+		Preconditions.checkNotNull(brick);
 		synchronized (this) {
 			brickToObjectsMap.remove(brick);
 		}
@@ -90,6 +93,8 @@ public final class MyxRegistry {
 	 * @return the brick with the given name, or <code>null</code> if none with that name are registered.
 	 */
 	public synchronized IMyxBrick getBrick(IMyxName name) {
+		Preconditions.checkNotNull(name);
+		Preconditions.checkNotNull(name.getName());
 		for (IMyxBrick brick : brickToObjectsMap.keySet()) {
 			if (name.equals(MyxUtils.getName(brick))) {
 				return brick;
@@ -106,6 +111,8 @@ public final class MyxRegistry {
 	 */
 	@SuppressWarnings("unchecked")
 	public <B extends IMyxBrick> B waitForBrick(IMyxName name) {
+		Preconditions.checkNotNull(name);
+		Preconditions.checkNotNull(name.getName());
 		IMyxBrick brick = null;
 		while ((brick = getBrick(name)) == null) {
 			synchronized (this) {
@@ -128,6 +135,7 @@ public final class MyxRegistry {
 	 */
 	@SuppressWarnings("unchecked")
 	public synchronized <B extends IMyxBrick> B getBrick(Class<B> brickClass) {
+		Preconditions.checkNotNull(brickClass);
 		for (IMyxBrick brick : brickToObjectsMap.keySet()) {
 			if (brickClass.equals(brick.getClass())) {
 				return (B) brick;
@@ -143,6 +151,7 @@ public final class MyxRegistry {
 	 * @return the brick of the given type.
 	 */
 	public <B extends IMyxBrick> B waitForBrick(Class<B> brickClass) {
+		Preconditions.checkNotNull(brickClass);
 		B brick = null;
 		while ((brick = getBrick(brickClass)) == null) {
 			synchronized (this) {
@@ -164,13 +173,15 @@ public final class MyxRegistry {
 	 * @param object The object to associate with the brick.
 	 */
 	public void registerObject(IMyxBrick brick, Object object) {
+		Preconditions.checkNotNull(brick);
+		Preconditions.checkNotNull(object);
 		synchronized (this) {
 			registerBrick(brick);
 			List<WeakReference<Object>> objectsList = brickToObjectsMap.get(brick);
 			objectsList.add(new WeakReference<>(object));
 			notifyAll();
+			fireMyxRegistryEvent(MyxRegistryEvent.EventType.OBJECT_REGISTERED, brick, object);
 		}
-		fireMyxRegistryEvent(MyxRegistryEvent.EventType.OBJECT_REGISTERED, brick, object);
 	}
 
 	/**
@@ -188,17 +199,21 @@ public final class MyxRegistry {
 	 * @param object The object to remove.
 	 */
 	public void unregisterObject(IMyxBrick brick, Object object) {
+		Preconditions.checkNotNull(brick);
+		Preconditions.checkNotNull(object);
 		synchronized (this) {
 			registerBrick(brick);
-			for (Iterator<WeakReference<Object>> i = brickToObjectsMap.get(brick).iterator(); i.hasNext();) {
-				Object weakObject = i.next().get();
+			List<WeakReference<Object>> weakObjects = brickToObjectsMap.get(brick);
+			for (WeakReference<Object> weakReference : weakObjects) {
+				Object weakObject = weakReference.get();
 				// Clean up garbage collected references.
 				if (weakObject == null) {
-					i.remove();
+					weakObjects.remove(weakReference);
+					continue;
 				}
 				if (object.equals(weakObject)) {
-					i.remove();
-					break;
+					weakObjects.remove(weakReference);
+					continue;
 				}
 			}
 		}
@@ -219,14 +234,17 @@ public final class MyxRegistry {
 	 * @return a list of all objects associated with the brick.
 	 */
 	public synchronized List<? extends Object> getObjects(IMyxBrick brick) {
+		Preconditions.checkNotNull(brick);
 		registerBrick(brick);
 		List<WeakReference<Object>> weakObjects = brickToObjectsMap.get(brick);
 		List<Object> objects = Lists.newArrayListWithCapacity(weakObjects.size());
 		for (Iterator<WeakReference<Object>> i = weakObjects.iterator(); i.hasNext();) {
-			Object weakObject = i.next().get();
+			WeakReference<Object> weakReference = i.next();
+			Object weakObject = weakReference.get();
 			// Clean up garbage collected references.
 			if (weakObject == null) {
-				i.remove();
+				weakObjects.remove(weakReference);
+				continue;
 			}
 			objects.add(weakObject);
 		}
@@ -243,14 +261,18 @@ public final class MyxRegistry {
 	 */
 	@SuppressWarnings("unchecked")
 	public <T> Iterable<T> getObjects(IMyxBrick brick, Class<T> objectClass) {
+		Preconditions.checkNotNull(brick);
+		Preconditions.checkNotNull(objectClass);
 		registerBrick(brick);
 		List<WeakReference<Object>> weakObjects = brickToObjectsMap.get(brick);
 		List<T> objects = Lists.newArrayListWithCapacity(weakObjects.size());
 		for (Iterator<WeakReference<Object>> i = weakObjects.iterator(); i.hasNext();) {
-			Object weakObject = i.next().get();
+			WeakReference<Object> weakReference = i.next();
+			Object weakObject = weakReference.get();
 			// Clean up garbage collected references.
 			if (weakObject == null) {
-				i.remove();
+				weakObjects.remove(weakReference);
+				continue;
 			}
 			if (objectClass.isInstance(weakObject)) {
 				objects.add((T) weakObject);
@@ -266,6 +288,7 @@ public final class MyxRegistry {
 	 * @param listener The listener.
 	 */
 	public synchronized void addMyxRegistryListener(IMyxRegistryListener listener) {
+		Preconditions.checkNotNull(listeners);
 		listeners.add(listener);
 	}
 
@@ -276,6 +299,7 @@ public final class MyxRegistry {
 	 * @param listener The listener.
 	 */
 	public synchronized void removeMyxRegistryListener(IMyxRegistryListener l) {
+		Preconditions.checkNotNull(listeners);
 		listeners.remove(l);
 	}
 
@@ -287,7 +311,9 @@ public final class MyxRegistry {
 	 * @param object The object that was added.
 	 */
 	private void fireMyxRegistryEvent(EventType eventType, IMyxBrick brick, Object object) {
-		assert!Thread.holdsLock(this);
+		Preconditions.checkNotNull(eventType);
+		Preconditions.checkNotNull(brick);
+		Preconditions.checkState(Thread.holdsLock(this));
 		MyxRegistryEvent evt = new MyxRegistryEvent(eventType, brick, object);
 		for (IMyxRegistryListener listener : listeners) {
 			listener.handleMyxRegistryEvent(evt);
