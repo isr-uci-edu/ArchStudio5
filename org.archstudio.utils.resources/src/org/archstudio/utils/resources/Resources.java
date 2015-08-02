3f008c99edbc850765f86d4bbe07a4989d1c2815
Scott A. Hendrickson <sahendrickson@gmail.com>
2015-08-02 16:44:50 -0700

    Added copy/cut/paste/delete icons.

--- org.archstudio.utils.resources/src/org/archstudio/utils/resources/Resources.java
+++ org.archstudio.utils.resources/src/org/archstudio/utils/resources/Resources.java
@@ -19,4 +19,16 @@ public class Resources {
 
   public static final URL FOLDER_ICON_8 =
       Activator.getContext().getBundle().getResource("res/folder-8.png");
+
+  public static final URL COPY_ICON_16 =
+      Activator.getContext().getBundle().getResource("res/copy_edit.png");
+
+  public static final URL CUT_ICON_8 =
+      Activator.getContext().getBundle().getResource("res/cut_edit.png");
+
+  public static final URL PASTE_ICON_16 =
+      Activator.getContext().getBundle().getResource("res/paste_edit.png");
+
+  public static final URL DELETE_ICON_16 =
+      Activator.getContext().getBundle().getResource("res/delete_edit.png");
 }
