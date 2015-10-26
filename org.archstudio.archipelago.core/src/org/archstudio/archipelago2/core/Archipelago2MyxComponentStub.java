864de3407c5aaae18f2a8a07309659d3e830bd33
Scott A. Hendrickson <sahendrickson@gmail.com>
2015-10-25 22:53:42 -0700

    Removed graph layout component.

--- org.archstudio.archipelago.core/src/org/archstudio/archipelago2/core/Archipelago2MyxComponentStub.java
+++ org.archstudio.archipelago.core/src/org/archstudio/archipelago2/core/Archipelago2MyxComponentStub.java
@@ -1,7 +1,6 @@
 package org.archstudio.archipelago2.core;
 
 import org.archstudio.myx.fw.IMyxName;
-import org.archstudio.myx.fw.MyxUtils;
 
 /*
  * DO NOT EDIT THIS CLASS, it is automatically generated. ANY MODIFICATIONS WILL BE OVERWRITTEN.
@@ -29,34 +28,6 @@ public abstract class Archipelago2MyxComponentStub
   }
 
   /**
-   * Myx name for the <code>graphLayout</code> interface.
-   * 
-   * @generated
-   */
-  public static final IMyxName OUT_GRAPH_LAYOUT = MyxUtils.createName("graphLayout");
-
-  /**
-   * Service object for the graphLayout interface.
-   * 
-   * @see #OUT_GRAPH_LAYOUT
-   * @generated
-   */
-  protected org.archstudio.graphlayout.IGraphLayout graphLayout = null;
-
-  /**
-   * Returns the service object(s) for the graphLayout interface.
-   * 
-   * @see #OUT_GRAPH_LAYOUT
-   * @generated
-   */
-  public org.archstudio.graphlayout.IGraphLayout getGraphLayout() {
-    if (graphLayout == null) {
-      throw new NullPointerException("Uninitialized service object: graphLayout");
-    }
-    return graphLayout;
-  }
-
-  /**
    * Returns service object(s) for IN interfaces.
    * 
    * @generated
@@ -69,7 +40,6 @@ public abstract class Archipelago2MyxComponentStub
   /**
    * Update service objects based on connected OUT interfaces.
    * 
-   * @see #OUT_GRAPH_LAYOUT
    * @generated
    */
   @Override
@@ -78,35 +48,18 @@ public abstract class Archipelago2MyxComponentStub
       throw new NullPointerException(interfaceName.getName());
     }
 
-    if (interfaceName.equals(OUT_GRAPH_LAYOUT)) {
-      if (graphLayout != null) {
-        throw new IllegalStateException(
-            "Only a single connection is supported on interface: graphLayout");
-      }
-      graphLayout = (org.archstudio.graphlayout.IGraphLayout) serviceObject;
-      return;
-    }
-
     super.interfaceConnected(interfaceName, serviceObject);
   }
 
   /**
    * Update service objects based on disconnecting OUT interfaces.
    * 
-   * @see #OUT_GRAPH_LAYOUT
    * @generated
    */
   @Override
   public void interfaceDisconnecting(IMyxName interfaceName, Object serviceObject) {
     if (serviceObject == null) {
       throw new NullPointerException(interfaceName.getName());
-    }
-    if (interfaceName.equals(OUT_GRAPH_LAYOUT)) {
-      if (graphLayout == null) {
-        throw new IllegalStateException("A connection was never made on interface: graphLayout");
-      }
-      graphLayout = null;
-      return;
     }
     super.interfaceDisconnecting(interfaceName, serviceObject);
   }
